9888c71dbd52af92c85bc0386d3d6d3efe529701
Sin(guy)/Cos(guy) <sarochphan.nik@student.mahidol.edu>
2026-02-02 10:06:33 +0000

    programming decorate

--- main-exercise.c
+++ main-exercise.c
@@ -9,58 +9,54 @@
 #include <string.h>
 #include "std_node.h"
 
-NodePtr create_node(int id) {
-    NodePtr n = (NodePtr) malloc(sizeof(StdNode));
-    if (!n) return NULL;
-    n->id = id;
-    snprintf(n->name, sizeof(n->name), "Student%d", id);
-    n->name[sizeof(n->name)-1] = '\0';
-    n->next = NULL;
-    return n;
-}
-
-void append_node(NodePtr *head, NodePtr node) {
-    if (!node) return;
-    if (!*head) { *head = node; return; }
-    NodePtr cur = *head;
-    while (cur->next) cur = cur->next;
-    cur->next = node;
-}
-
-void print_list(const NodePtr head) {
-    NodePtr cur = head;
-    while (cur) {
-        printf("id=%d name=%s\n", cur->id, cur->name);
-        cur = cur->next;
-    }
-}
-
-void free_list(NodePtr head) {
-    NodePtr cur = head;
-    while (cur) {
-        NodePtr next = cur->next;
-        free(cur);
-        cur = next;
-    }
-}
-
 int main(int argc, char *argv[]) {
     if (argc <= 1) {
-        printf("Usage: %s <id> [id ...]\n", argv[0]);
+        printf("Usage: %s <id> <name> [<id> <name> ...]\n", argv[0]);
         return 0;
     }
 
-    NodePtr head = NULL;
-    for (int i = 1; i < argc; ++i) {
+    NodePtr head = NULL, tail = NULL, tmp;
+    int i = 1;
+    while (i < argc) {
         int id = atoi(argv[i]);
-        NodePtr n = create_node(id);
-        if (!n) { fprintf(stderr, "Allocation failed\n"); free_list(head); return 1; }
-        append_node(&head, n);
+        const char *name = NULL;
+        if (i + 1 < argc) name = argv[i+1];
+
+        NodePtr n = (NodePtr) malloc(sizeof(StdNode));
+        if (!n) { fprintf(stderr, "Allocation failed\n");
+            tmp = head; while (tmp) { head = head->next; free(tmp); tmp = head; };
+            return 1;
+        }
+        n->id = id;
+        if (name) {
+            strncpy(n->name, name, sizeof(n->name)-1);
+            n->name[sizeof(n->name)-1] = '\0';
+        } else {
+            snprintf(n->name, sizeof(n->name), "Student%d", id);
+            n->name[sizeof(n->name)-1] = '\0';
+        }
+        n->next = NULL;
+
+        if (!head) { head = n; tail = n; }
+        else { tail->next = n; tail = n; }
+
+        i += (name ? 2 : 1);
     }
 
     printf("List contents:\n");
-    print_list(head);
+    tmp = head;
+    while (tmp) {
+        printf("%d %s\n", tmp->id, tmp->name);
+        tmp = tmp->next;
+    }
 
-    free_list(head);
+    tmp = head;
+    while (tmp) {
+        head = head->next;
+        printf("Freeing %d %s\n", tmp->id, tmp->name);
+        free(tmp);
+        tmp = head;
+    }
+
     return 0;
 }
